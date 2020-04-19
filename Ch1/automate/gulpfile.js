10da3c183490854d0d077edd6f291bf83057dcee
Dominic Myers <annoyingmouse@gmail.com>
2020-04-19 08:39:35 +0100

    Called sass differently as per version 4
    Lost the console message I'm afraid

--- Ch1/automate/gulpfile.js
+++ Ch1/automate/gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp');
 var sass = require('gulp-dart-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-
  
 gulp.task('sass', function () {
  return gulp.src('sass/*.scss')
@@ -12,18 +11,10 @@ gulp.task('sass', function () {
   .pipe(sourcemaps.init('/maps'))
   .pipe(sass().on('error', sass.logError))
   .pipe(sourcemaps.write('./maps'))
-  .pipe(gulp.dest('./css'));
+  .pipe(gulp.dest('./css'))
+  .on('finish', () => console.log('SASS invoked and finished successfully'));
 });      
 
 gulp.task('default', function () {
-  gulp.watch('sass/*.scss', ['sass'])
-  .on('change', function(evt) {
-    console.log(
-      '\n[watcher] File ' + evt.path.replace(/.*(?=sass)/,'') + 
-      ' was ' + evt.type + ', compiling...'
-    );
-  });
+  gulp.watch('sass/*.scss', gulp.series('sass'));
 });
-
-
-	
